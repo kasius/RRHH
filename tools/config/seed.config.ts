6281606a3b4908c0ec0f104ef7f40be979953486
Carlos Cifuentes U <ccifuentes@iqs.cl>
2016-12-21 18:30:44 -0300

    Implementación Layout Principal

--- tools/config/seed.config.ts
+++ tools/config/seed.config.ts
@@ -144,7 +144,7 @@ export class SeedConfig {
    * `index.html`.
    * @type {string}
    */
-  APP_TITLE = 'Welcome to angular2-seed!';
+  APP_TITLE = 'Relay Race 2 &trade;';
 
   /**
    * The base folder of the applications source files.
